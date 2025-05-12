916fd7450c84421fbd937a31c070379011966b99
radekbrep <radekbeneda@gmail.com>
2025-05-12 21:45:19 +0000

    test server ok

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 const PORT = 8080;
 
 // Serve static files (e.g. scratchpad/index.html and JS)
-app.use(express.static('./scratchpad'));
+app.use(express.static('./source'));
 
 // Optional: fallback route
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'scratchpad/index.html'));
-//});
+ app.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname, 'scratchpad/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Dev server running at http://localhost:${PORT}`);
